f55693a00a1b2d0496bc75c52f174998e588d19b
Kyle <email@kylenoad.com>
2025-02-18 09:22:45 +0000

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <Header />
+      <NavBar />
       <Routes>
-        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/home" element={<Homepage />} />
       </Routes>
     </>
   );
