7dbf87ce17435925ca22fddaa3f52b6cf46c8309
Kyle <email@kylenoad.com>
2025-02-20 16:21:16 +0000

    added sorting functionality

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,31 +3,29 @@ import { getArticles } from "../api";
 import { useParams } from "react-router-dom";
 import TopicsFilter from "./TopicsFilter";
 import ArticleCardGrid from "./ArticleCardGrid";
+import SortArticles from "./SortArticles";
 
 function Homepage() {
   const { topic } = useParams();
   const [articles, setArticles] = useState([]);
+  const [sortBy, setSortBy] = useState("created_at");
   const [order, setOrder] = useState("desc");
 
   useEffect(() => {
-    getArticles(topic, "votes", order).then((response) => {
+    getArticles(topic, sortBy, order).then((response) => {
       setArticles(response.data.articles);
     });
-  }, [topic, order]);
+  }, [topic, sortBy, order]);
 
   return (
     <>
       <TopicsFilter />
-      <div className="flex space-x-5 mt-4 justify-center">
-        <select
-          value={order}
-          onChange={(event) => setOrder(event.target.value)}
-          className="bg-gray-200 border border-gray-300 rounded py-2 px-4"
-        >
-          <option value="asc">Votes Ascending</option>
-          <option value="desc">Votes Descending</option>
-        </select>
-      </div>
+      <SortArticles
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        order={order}
+        setOrder={setOrder}
+      />
       <ArticleCardGrid articles={articles} />
     </>
   );
