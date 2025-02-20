21fe8d47481a7bd61015f3a87ccf602ea4fb9248
Kyle <email@kylenoad.com>
2025-02-20 15:05:55 +0000

    started sorting functionality

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,19 +7,29 @@ import ArticleCardGrid from "./ArticleCardGrid";
 function Homepage() {
   const { topic } = useParams();
   const [articles, setArticles] = useState([]);
+  const [order, setOrder] = useState("desc");
 
   useEffect(() => {
-    getArticles(topic).then((response) => {
+    getArticles(topic, "votes", order).then((response) => {
       setArticles(response.data.articles);
     });
-  }, [topic]);
+  }, [topic, order]);
 
   return (
     <>
       <TopicsFilter />
+      <div className="flex space-x-5 mt-4 justify-center">
+        <select
+          value={order}
+          onChange={(event) => setOrder(event.target.value)}
+          className="bg-gray-200 border border-gray-300 rounded py-2 px-4"
+        >
+          <option value="asc">Votes Ascending</option>
+          <option value="desc">Votes Descending</option>
+        </select>
+      </div>
       <ArticleCardGrid articles={articles} />
     </>
   );
 }
-
 export default Homepage;
